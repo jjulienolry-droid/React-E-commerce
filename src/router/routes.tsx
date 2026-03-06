eca701b8c6aef2963cc84709c8daf53a923489fd
Julien <julien.olry@ccicampus.fr>
2026-03-06 02:13:56 +0100

    maquette + charte graphique

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom"
-import { MainLayout } from "../components/layout/MainLayout"
 import { ProtectedRoute } from "./ProtectedRoute"
 import { Home } from "../pages/Home"
 import { ProductDetailPage } from "../pages/ProductDetailPage"
